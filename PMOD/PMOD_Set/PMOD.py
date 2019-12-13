1293c5f8fda66529e8db0e57b3da102e379d85cd
Dolcwan <51341410+Dolcwan@users.noreply.github.com>
2019-12-12 18:44:04 -0800

    PMOD classes

---
+++ PMOD/PMOD_Set/PMOD.py
@@ -0,0 +1,75 @@
+import numpy as np
+import scipy as sp
+import math as mt
+import time
+
+
+class tcheck:
+
+    def __init__(self,var=None):
+        
+        self.var = var 
+        self.type = type(self.var)
+        
+    def set_var(self,var):
+        self.var = var 
+        self.type = type(var)
+
+    def str_test(self,var):
+        str_inst = isinstance(var,str)
+        return str_inst
+
+    def name_return(self,name):
+
+        if(name != None):
+            type = self.str_test(name)
+            if(type):
+                var_name = name
+            else:
+                var_name = None 
+        else:
+            var_name = None
+
+        return var_name 
+    
+
+    def numeric_test(self,var,name=None):
+        int_inst = isinstance(var, int)
+        float_inst = isinstance(var, float)
+        long_inst = isinstance(var, long)
+
+        numeric_bool = int_inst or float_inst or long_inst
+        
+        var_name = self.name_return(name)
+        
+        result = (numeric_bool,var_name)
+        return result 
+         
+         
+    def type_test(self,var,sort,name=None):             
+        type_bool = isinstance(var, sort) 
+        var_name = self.name_return(name)    
+        result = (type_bool,var_name)
+        return result 
+
+    
+    def fail_print(self,print_bool,result,correct_type='valid type.',func_name=''):
+        if(print_bool):
+            if(self.str_test(correct_type)):
+                correct_type = correct_type 
+            else:
+                try:
+                    correct_type = str(correct_type)
+                except:
+                    correct_type = correct_type
+            success = result[0]
+            name = result[1]
+            if(not success and name != None):
+                print("["+func_name+"]"+" TypeError: the variable '"+name+"' is not a "+correct_type)
+            if(not success and name == None):
+                print("["+func_name+"]"+" TypeError: input variable is not a "+correct_type)            
+
+             
+    def type_test_print(self,var,sort,name=None,func_name='',print_bool=True):
+        result = self.type_test(var,sort,name)
+        self.fail_print(print_bool,result,sort,func_name)
---
+++ PMOD/PMOD_Set/ioparse.py
@@ -0,0 +1,330 @@
+import tcheck 
+
+class ioparse:
+
+    def __init__(self, file_in=None, file_out=None, pyver='27'):
+        global check
+        self.file_in = file_in 
+        self.file_out = file_out
+
+        self.pyver = pyver
+
+        check = tcheck.tcheck()
+
+        self.ptype_list = ['r','r+','rb','w','w+','wb','wb+','a','ab','a+','ab+']
+        self.ptype_read = ['r','r+','rb']
+        self.ptype_write = ['w','w+','wb','wb+','a','ab','a+','ab+']
+
+    
+    def set_file(self,file_in,file_out):
+        self.file_in = file_in 
+        self.file_out = file_out  
+
+    def dup_check(self,array):
+        '''If there is a duplicate in array-like object, True is returned, else False'''    
+        s = set()
+        for i in array:
+            if i in s:
+                return True
+            else:
+                s.add(i)         
+        return False        
+
+
+    def attempt_path_print(self,filepath):
+        try:
+            print("Given pathway: "+str(filepath))
+        except:
+            print("Pathway could not be parsed") 
+
+
+    def io_opt_test(self, ptype,io,par=False,count_offset=False):
+        
+        success = True
+
+        if(ptype not in self.ptype_list):
+            success = False
+            try:            
+                print("[io_opt_test] Error: 'ptype', '"+str(ptype)+"' is not a valid 'ptype' option")
+            except:
+                print("[io_opt_test] Error: 'ptype', could not be parsed as a valid 'ptype' option string")   
+
+        if(io == 'read'):
+            if(ptype not in self.ptype_read):
+                success = False                            
+                print("[io_opt_test] Error: ptype, '"+str(ptype)+"' is not a valid read option")
+        elif(io == 'write'):
+            if(ptype not in self.ptype_write):   
+                success = False                         
+                print("[io_opt_test] Error: ptype, '"+str(ptype)+"' is not a valid write option")      
+        else:
+            success = False
+            print("[io_opt_test] Error: option 'io' must be either 'read' or 'write'")
+        
+        test = check.type_test_print(par,bool,'par','io_opt_test') 
+        if(not test):
+            success = False 
+             
+        test = check.type_test_print(count_offset,bool,'count_offset','io_opt_test') 
+        if(not test):
+            success = False 
+                  
+        return success
+  
+    
+    def flat_file_read(self,file_in,ptype='r'):
+        '''
+        flat_file_read(self,file_in,ptype='r')
+
+        Description: Writes a list of strings (1 line per string) from an input file, various options for parsing
+          
+        (e.g.)
+        flat_file_read('file.in',ptype='rb')   
+
+        Variables:
+        
+        'file_in': file string pathway, if only a single node is given, current (path) directory is assumed
+        'ptype': [*] a string in found in the self.ptype_read list.         
+
+        Output: List of Strings; else Boolean if failure           
+         
+        '''    
+
+        test = io_opt_test(ptype,'read')
+        if(not test):
+            return False            
+
+        test = check.type_test_print(file_in,str,'file_in','flat_file_read') 
+        if(not test):
+            return False
+      
+        try:
+            with open(file_in,ptype) as file_in:
+                file_lines = file_in.readlines()
+            return file_lines 
+        except:
+            print("[flat_file_read] Error: File could not be read")
+            self.attempt_path_print(file_in)
+            return False             
+        
+
+    def flat_file_write(self,file_out,add_list,par=False,ptype='w'):
+        '''
+        flat_file_write(self,file_out,add_list,ptype='w',par=False)
+
+        Description: Writes a list of strings to an output file, various options for parsing
+
+        (e.g.)
+        flat_file_write('file.in',["This is the first line!","This is line #2!"])
+
+        Variables:
+        
+        'file_out': file string pathway, if only a single node is given, current (path) directory is assumed
+        'add_list': list of strings, each string is a separate line, order denoted by the index. 
+        'ptype': [*] a string in found in the self.ptype_write list.         
+        'par': [*] True if endline character is to be added to each output string, else False. 
+
+        Output: Boolean      
+
+        ''' 
+
+        # Testing proper variable types
+        test = io_opt_test(ptype,'write',par=par)
+        if(not test):
+            return False         
+
+        test = check.type_test_print(file_out,str,'file_out','flat_file_write') 
+        if(not test):
+            return False
+        test = check.type_test_print(add_list,list,'add_list','flat_file_write') 
+        if(not test):
+            return False
+        
+        n=len(add_list)
+        for i in range(n):
+            test = check.type_test_print(add_list[i],str,'add_list['+str(i)+']','flat_file_write')
+            if(not test):
+                return False           
+            
+        # Print content to file   
+        try: 
+            with open(file_out,ptype) as fout:
+                for i in add_list: 
+                    if(par):
+                        fout.write(i+"\n")
+                    else:
+                        fout.write(i) 
+            return True 
+        except: 
+            print("[flat_file_write] Error: 'add_list' lines could not be printed to file")
+            self.attempt_path_print(file_out)
+            return False
+
+
+
+    def flat_file_replace(self,file_out,grab_list,change_list,count_offset=True,par=True,ptype='w'):
+        '''
+        flat_file_replace(file_out,grab_list,change_list,par=True,count_offset=True,ptype='w')
+
+        Description: In the file 'file_out', the lines in 'grab_list' are replaced with the strings in 'change_list'
+
+        (e.g.)
+        flat_file_replace('file.in',[1,2],["This is the first line!","This is line #2!"])
+
+        Variables:
+        
+        'file_out': file string pathway, if only a single node is given, current (path) directory is assumed
+        'grab_list': list of integers, each integer corrosponds to a line number, options for 0 or 1 index start 
+        'change_list': list of strings, each string is a separate line
+        'par': [*] True if endline character is to be added to each output string, else False. 
+        'count_offset': [*] True if values in grab_list corrospond to line numbers, else values corrospond to list index
+        'ptype': [*] a string in found in the self.ptype_write list.         
+
+        Output: Boolean   
+        '''
+        
+        # Testing proper variable types
+        test = io_opt_test(ptype,io,par=par,count_offset=count_offset)
+        if(not test):
+            return False
+
+        test = check.type_test_print(file_out,str,'file_out','flat_file_replace') 
+        if(not test):
+            return False
+        test = check.type_test_print(grab_list,list,'grab_list','flat_file_replace')
+        if(not test):
+            return False
+
+        for i in range(len(grab_list)):
+            test = check.type_test_print(grab_list[i],int,'grab_list['+str(i)+']','flat_file_replace') 
+            if(not test):
+                return False 
+        for i in range(len(change_list)):
+            test = check.type_test_print(change_list[i],str,'change_list['+str(i)+']','flat_file_replace') 
+            if(not test):
+                return False 
+
+        test = self.dup_check(grab_list)
+        if(test):
+            print("[flat_file_replace] Error: 'grab_list' values must be unique")
+
+        # Read in file
+        file_lines = self.flat_file_read(file_out)
+    
+        # Testing the lengths of variable arrays 
+        m = len(file_lines)
+        n = len(grab_list)
+
+        if(count_offset):      
+            grab_list = [x-1 for x in grab_list]
+
+        if(len(grab_list) != len(change_list)):  
+            print("[flat_file_replace] Error: 'grab_list' and 'change_list' must have the same length")
+            return False
+
+        if(n>m):
+            print("[flat_file_replace] Error: 'grab_list' is longer than the number of file lines")
+            return False
+        if(m < (max(grab_list)+1)):
+            print("[flat_file_replace] Error: replacement line number greater than the max number of files lines")
+            return False
+            
+        # Make modifications 
+        for i in grab_list:
+            j = grab_list.index(i)
+            if(par):
+                file_lines[i] = change_list[j]+"\n"
+            else:
+                file_lines[i] = change_list[j]
+        
+        # Print modifications to file 
+        result = self.flat_file_write(file_out,file_lines,ptype=ptype)
+        return result
+        
+
+
+    def flat_file_grab(file_in,grab_list,scrub=False,repeat=False,count_offset=True,ptype='w'):
+        
+        n = len(grab_list)
+
+        # Testing proper variable types
+        test = io_opt_test(ptype,'read',count_offset=count_offset)
+        if(not test):
+            return False
+
+        test = check.type_test_print(file_in,str,'file_in','flat_file_grab') 
+        if(not test):
+            return False
+        test = check.type_test_print(grab_list,list,'grab_list','flat_file_grab')
+        if(not test):
+            return False
+
+        if(n>0):
+            for i in range(n):
+                test = check.type_test_print(grab_list[i],int,'grab_list['+str(i)+']','flat_file_grab') 
+                if(not test):
+                    return False 
+
+        if(repeat == False):
+            test = self.dup_check(grab_list)
+            if(test):
+                print("[flat_file_grab] Error: 'grab_list' values must be unique")
+            return False
+
+        # Read in file
+        file_lines = self.flat_file_read(file_in)
+        if(n == 0):
+            if(scrub == True):
+                for i in range(len(file_lines)):
+                    file_lines[i] = file_lines[i].strip('\n').strip('\r')
+                return file_lines   
+            else:
+                return file_lines     
+
+        # Testing the lengths of variable arrays 
+        m = len(file_lines)
+
+        if(count_offset):      
+            grab_list = [x-1 for x in grab_list]
+
+        if(len(grab_list) != len(change_list)):  
+            print("[flat_file_grab] Error: 'grab_list' and 'change_list' must have the same length")
+            return False
+
+        if(n>m):
+            print("[flat_file_grab] Error: 'grab_list' is longer than the number of file lines")
+            return False
+        if(m < (max(grab_list)+1)):
+            print("[flat_file_grab] Error: replacement line number greater than the max number of files lines")
+            return False
+
+        # parse file_lines through repeat options:
+                
+        out_lines = [] 
+
+        if(repeat == False):
+            for i in range(n):
+                out_lines.append(file_lines[grab_list[i]])     
+                if(scrub == True):
+                    out_lines[i] = out_lines[i].strip("\n").strip("\r")
+            return out_lines  
+
+        raw_lines = []
+        form_lines = []                              
+                    
+        if(repeat == True):  
+            assert len(grab_list) > 2, "Error: grab_list must take at least three values"
+            bnd = grab_list[0]+1
+            saut = grab_list[1:-1]
+            n = grab_list[-1]+1
+            for i in range(n):
+                for j in range(len(saut)):
+                    line_tag = saut[j]+bnd*i
+                    raw_lines.append(file_lines[line_tag])
+                    lines = file_lines[line_tag].strip("\n").strip("\r").split(" ")            
+                    lines = filter(None,lines)          
+                    form_lines.append(lines)            
+            if(scrub == True):
+                return form_lines
+            else:
+                return raw_lines                    
